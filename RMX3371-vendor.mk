5cc7018028719ecd07c51d34f92c6d5e0c2fa930
ryukftw <ryukv009@gmail.com>
2025-04-13 04:56:26 +0000

    RMX3371: Patch camera blobs to use libstdc++_vendor
    Signed-off-by: ryukftw <ryukv009@gmail.com>

--- RMX3371-vendor.mk
+++ RMX3371-vendor.mk
@@ -1599,7 +1599,6 @@ PRODUCT_PACKAGES += \
     libsnpeml \
     libstblur_api \
     libstblur_capture_api \
-    libstdc++ \
     libstface_fd_api \
     libstfaceunlockocl \
     libstfd_mobile_api \
